497534ea844e9e6e5c41d38c10a78e97a49f13b5
yeyun0423 <pet2ytty@gmail.com>
2024-11-13 15:23:37 +0900

    인라인 css 작성

--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Hello = () => {
-  return <p>Hello!</p>;
+  return (
+    <p>
+      <h1
+        style={{
+          color: "#f00",
+          borderRight: "12px solid #000",
+          marginBottom: "50px",
+          opacity: 0.5,
+        }}
+      >
+        {" "}
+        Hello!
+      </h1>
+    </p>
+  );
 };
 
 export default Hello;
