f0c04d28a6055b693151c6ffbf1aa147dc93aba4
yeyun0423 <pet2ytty@gmail.com>
2024-11-13 17:16:08 +0900

    이벤트 처리 완료

--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./Hello.module.css";
 const Hello = () => {
+  function ShowName() {
+    console.log("Mike");
+  }
+
+  function Change(e) {
+    console.log(e.target.value);
+  }
   return (
     <div>
       <h1>Hello!</h1>
-      <div className={styles.box}>ok</div>
+      <button onClick={ShowName}>ShowName</button>
+      <button onClick={() => console.log(30)}>Showage</button>
+      <input onChange={Change}></input>
     </div>
   );
 };
